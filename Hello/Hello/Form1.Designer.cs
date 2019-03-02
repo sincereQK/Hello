5a38a97f08d757413d043087ce4560eed152bc6f
sincereQK <mattmingyu@gmail.com>
2019-03-02 19:14:10 +0900

    민규 첫번째 Git 사용 Hello world 작성

---
+++ Hello/Hello/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Hello
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHello = new System.Windows.Forms.Label();
+            this.buttExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblHello
+            // 
+            this.lblHello.Font = new System.Drawing.Font("굴림", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblHello.Location = new System.Drawing.Point(205, 143);
+            this.lblHello.Name = "lblHello";
+            this.lblHello.Size = new System.Drawing.Size(369, 97);
+            this.lblHello.TabIndex = 0;
+            this.lblHello.Text = "label1";
+            // 
+            // buttExit
+            // 
+            this.buttExit.Location = new System.Drawing.Point(210, 292);
+            this.buttExit.Name = "buttExit";
+            this.buttExit.Size = new System.Drawing.Size(75, 23);
+            this.buttExit.TabIndex = 1;
+            this.buttExit.Text = "Exit";
+            this.buttExit.UseVisualStyleBackColor = true;
+            this.buttExit.Click += new System.EventHandler(this.buttExitClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttExit);
+            this.Controls.Add(this.lblHello);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHello;
+        private System.Windows.Forms.Button buttExit;
+    }
+}
+
